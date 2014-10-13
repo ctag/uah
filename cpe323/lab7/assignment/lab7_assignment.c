53a571449c62906a88b6566d6a74c1ef9087e032
Christopher Bero <bigbero@gmail.com>
2014-10-13 18:25:53 -0500

    Done working at the lab for today, lab7 is a go

--- cpe323/lab7/assignment/lab7_assignment.c
+++ cpe323/lab7/assignment/lab7_assignment.c
@@ -29,21 +29,21 @@
 #define LED2 BIT1; // LED2 - P2.1 - 0x02
 
 // Usage: WDTCTL = WDT_CONFIG;
-#define WDT_CONFIG (WDTPW|WDTCNTCL|WDTSSEL) // Set bits to give us 1s watchdog
-#define WDT_INTERVAL (WDTPW|WDTCNTCL|WDTTMSEL|WDTSSEL|WDTIS0) // Set bits to have a 1s timer
+#define WDT_CONFIG (WDTPW|WDTCNTCL|WDTSSEL|WDTIS0) // Set bits to give us 0.250s watchdog
+#define WDT_INTERVAL (WDTPW|WDTCNTCL|WDTTMSEL|WDTSSEL|WDTIS0) // Set bits to have 0.250s timer
 #define WDT_HALT (WDTPW|WDTHOLD) // Set bits to halt the timer
 
 short unsigned int new_note_flag = 0;
 
-double freq[] = {440.00, 493.88, 523.25, 587.33, 659.25, 698.46, 783.99};
-char notes[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+double freq[] = {440.00, 493.88, 523.25, 587.33, 659.25, 698.46, 783.99, 1};
+char notes[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', ' '};
 
-char mysong[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+char mysong[] = {'C', 'C', ' ', 'C', 'C', ' ', 'G', 'G', ' ', 'G', 'G', ' ', 'A', 'A', ' ', 'A', 'A', ' ', 'G', 'G', ' ', 'F', 'F', ' ', 'F', 'F', ' ', 'E', 'E', ' ', 'E', 'E', ' ', 'D', 'D', ' ', 'D', 'D', ' ', 'C', 'C', ' ', ' ', ' ', ' '};
 
 int index = 0;
 
-int notes_len = 7;
-int mysong_len = 7;
+int notes_len = 8;
+int mysong_len = 45;
 
 char note;
 
@@ -56,56 +56,59 @@ void main(void)
 	P3DIR |= BIT5; // Set P3.5 to OUT
 	P3SEL |= BIT5; // Setup the special TB4 function of P3.5
 	
-	/* // Setup Timer_B's TBCTL
-	// BITs Mapping for TxCTLx:
-	//	[15]	[14]	[13]	[12]	[11]	[10]	[9]		[8]
-	//	NA		NA		NA		NA		NA		NA		TASSEL	TASSEL
-	//	0		0		0		0		0		0		0		1
-	//
-	//	[7]		[6]		[5]		[4]		[3]		[2]		[1]		[0]
-	//	ID		ID		MC		MC		NA		TACLR	TAIE	TAIFG
-	//	0		1		0		1		0		0		0		0
-	//
-	// Condensed: [0000][0001] [0101][0000]
-	//
-	// Specifics:
-	// TASSELx = 01, ACLK
-	// IDx = 01, divide ACLK by 2, ACLK/2 = 16,384
-	// MCx = 01, UP mode
-	*/
+	/* 
+	 * Setup Timer_B's TBCTL
+	 * BITs Mapping for TxCTLx:
+	 *	[15]	[14]	[13]	[12]	[11]	[10]	[9]		[8]
+	 *	NA		NA		NA		NA		NA		NA		TASSEL	TASSEL
+	 *	0		0		0		0		0		0		0		1
+	 *
+	 *	[7]		[6]		[5]		[4]		[3]		[2]		[1]		[0]
+	 *	ID		ID		MC		MC		NA		TACLR	TAIE	TAIFG
+	 *	0		1		0		1		0		0		0		0
+	 *
+	 * Condensed: [0000][0001] [0101][0000]
+	 *
+	 * Specifics:
+	 * TASSELx = 01, ACLK
+	 * IDx = 01, divide ACLK by 2, ACLK/2 = 16,384
+	 * MCx = 01, UP mode
+	 */
 	TBCTL = 0x0150;
 	
-	/* // Choosing a value for TBCCR0:
-	// This is a little tricky, because you can tackle this problem
-	// from a multitude of angles, and the one I chose is likely non-standard,
-	// but makes much more sense to me.
-	// So, TBCCR0 represents the period out of 16,384 which we wish
-	// for the speaker to be pulsed each second. A value of 16,384 means
-	// the speaker will click once per second. A value of 16 means the
-	// speaker will click about 1000 times per second, which is an audible tone!
-	//
-	// The formula:
-	// TBCCR0 = [ACLK = 16384] / [desired freq in Hz]
-	//
-	// So, for 900Hz, TBCCR0 = [16384] / [900] = 18.2 = 18.
-	*/
+	/* 
+	 * Choosing a value for TBCCR0:
+	 * This is a little tricky, because you can tackle this problem
+	 * from a multitude of angles, and the one I chose is likely non-standard,
+	 * but makes much more sense to me.
+	 * So, TBCCR0 represents the period out of 16,384 which we wish
+	 * for the speaker to be pulsed each second. A value of 16,384 means
+	 * the speaker will click once per second. A value of 16 means the
+	 * speaker will click about 1000 times per second, which is an audible tone!
+	 *
+	 * The formula:
+	 * TBCCR0 = [ACLK = 16384] / [desired freq in Hz]
+	 *
+	 * So, for 900Hz, TBCCR0 = [16384] / [900] = 18.2 = 18.
+	 */
 	TBCCR0 = 19;
 	
-	/* // Setup Timer_B's TBCCTL4
-	// BITs Mapping for TxCTLx:
-	//	[15]	[14]	[13]	[12]	[11]	[10]	[9]		[8]
-	//	CM		CM		CCIS	CCIS	SCS		CCLD	CCLD	CAP
-	//	0		0		0		0		0		0		0		0
-	//
-	//	[7]		[6]		[5]		[4]		[3]		[2]		[1]		[0]
-	//	OUTMOD	OUTMOD	OUTMOD	CCIE	CCI		OUT		COV		CCIFG
-	//	1		0		0		0		0		0		0		0
-	//
-	// Condensed: [0000][0000] [1000][0000]
-	//
-	// Specifics:
-	// OUTx = 100, Toggle mode (on TB4 of course) 
-	*/
+	/*
+	 * Setup Timer_B's TBCCTL4
+	 * BITs Mapping for TxCTLx:
+	 *	[15]	[14]	[13]	[12]	[11]	[10]	[9]		[8]
+	 *	CM		CM		CCIS	CCIS	SCS		CCLD	CCLD	CAP
+	 *	0		0		0		0		0		0		0		0
+	 *
+	 *	[7]		[6]		[5]		[4]		[3]		[2]		[1]		[0]
+	 *	OUTMOD	OUTMOD	OUTMOD	CCIE	CCI		OUT		COV		CCIFG
+	 *	1		0		0		0		0		0		0		0
+	 *
+	 * Condensed: [0000][0000] [1000][0000]
+	 *
+	 * Specifics:
+	 * OUTx = 100, Toggle mode (on TB4 of course) 
+	 */
 	TBCCTL4 = 0x0080;
 	
 	TBCCR4 = 1; // doesn't matter, can be any valid value
@@ -122,7 +125,7 @@ void main(void)
 		{
 			if (notes[i] == note)
 			{
-				TBCCR0 = (16384 / freq[i]);
+				TBCCR0 = (16384 / (int)freq[i]);
 				break;
 			}
 		}
