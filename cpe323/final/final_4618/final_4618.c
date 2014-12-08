edffefaca59e9b0ad404bccc92a69b9076d91c85
Christopher Bero <bigbero@gmail.com>
2014-12-08 11:07:13 -0600

    done with final lab

--- cpe323/final/final_4618/final_4618.c
+++ cpe323/final/final_4618/final_4618.c
@@ -39,7 +39,7 @@ short unsigned int rsy_percent = 0;
 
 char current_char = '\0';
 
-int alpha_index [3][4] = {
+int alpha_index [4][3] = {
 	-1, 0, -1,
 	1, -1, 2,
 	3, -1, 4,
@@ -91,9 +91,9 @@ void UART_Write(char c)
 	UCA0TXBUF = c;
 } 
 
-void Generate_Section(short unsigned int input, char axis)
+int Generate_Section(short unsigned int input, char axis)
 {
-	if (axis = 'x') {
+	if (axis == 'x') {
 		if (input < 33) {
 			return (0);
 		} else if (input >= 33 && input <= 66) {
@@ -112,30 +112,39 @@ void Generate_Section(short unsigned int input, char axis)
 			return (3);
 		}
 	}
+	return (-1);
 }
 
 void UART_Send_Char(void)
 {
-	lsx_percent = (lsx*3/4095*100/3);
-	lsy_percent = (lsy*3/4095*100/3);
-	rsx_percent = (rsx*3/4095*100/3);
-	rsy_percent = (rsy*3/4095*100/3);
+	lsx_percent = (int)(lsx*(3.40/4095)*100/3.40);
+	lsy_percent = (int)(lsy*3.4/4095*100/3.4);
+	rsx_percent = (int)(rsx*3.4/4095*100/3.4);
+	rsy_percent = (int)(rsy*3.4/4095*100/3.4);
 	
 	int lsx_loc = Generate_Section(lsx_percent, 'x');
 	int lsy_loc = Generate_Section(lsy_percent, 'y');
 	int rsx_loc = Generate_Section(rsx_percent, 'x');
 	int rsy_loc = Generate_Section(rsy_percent, 'y');
 	
-	if ((alpha_index[lsx_loc][lsy_loc] == -1) || (alpha_index[rsx_loc][rsy_loc] == -1)) {
+	int l_index = alpha_index[lsy_loc][lsx_loc];
+	int r_index = alpha_index[rsy_loc][rsx_loc];
+	
+	if ((l_index == -1) || (r_index == -1)) {
+		//UART_Write('_');
+	  	current_char = '\0';
 		return;
 	}
 	
-	int l_index = alpha_index[lsx_loc][lsy_loc];
-	int r_index = alpha_index[rsx_loc][rsy_loc];
+	if (current_char != '\0') {
+	  return;
+	}
+	
+	current_char = alphabet[r_index][l_index];
 	
 	if (lsx > 50 && rsx > 50)
 	{
-		UART_Write(alphabet[l_index][r_index]);
+		UART_Write(current_char);
 	}
 }
 
@@ -152,15 +161,15 @@ void main(void)
 	// Setup Timer_A
 	TACCR0 = 3277;				// 3277 / 32768 Hz = 0.1s
 	TACTL = TASSEL_1 + MC_1;	// ACLK, up mode  
-	//TACCTL0 = CCIE;				// enabled interrupt
+	TACCTL0 = CCIE;				// enabled interrupt
 	
 	// Setup ADC 12
 	ADC12CTL0 = ADC12ON + SHT0_8 + MSC;	// configure ADC converter
 	ADC12CTL1 = SHP + CONSEQ_1;		// Use sample timer, single sequence
-	ADC12MCTL0 = INCH_4;			// ADC channel 0 is A4 pin - Left-Stick X-axis
-	ADC12MCTL1 = INCH_5;			// ADC channel 1 is A5 pin - Left-Stick Y-axis
-	ADC12MCTL2 = INCH_6;			// ADC channel 2 is A6 pin - Right-Stick X-axis
-	ADC12MCTL3 = INCH_7 + EOS;		// ADC channel 3 is A7 pin - Right-Stick Y-axis
+	ADC12MCTL0 = INCH_0 + SREF_2;			// ADC channel 0 is A0 pin - Left-Stick X-axis
+	ADC12MCTL1 = INCH_3 + SREF_2;			// ADC channel 1 is A3 pin - Left-Stick Y-axis
+	ADC12MCTL2 = INCH_4 + SREF_2;			// ADC channel 2 is A4 pin - Right-Stick X-axis
+	ADC12MCTL3 = INCH_7 + SREF_2 + EOS;		// ADC channel 3 is A7 pin - Right-Stick Y-axis
 	// EOS - End of Sequence for Conversions
 	
 	ADC12IE |= 0x0008;	// Enable ADC12 Interrupts
@@ -169,7 +178,7 @@ void main(void)
 	
 	_EINT();	// Enable interrupts
 	
-	//_BIS_SR(LPM0_bits + GIE);         // Enter LPM0 w/ interrupt
+	_BIS_SR(LPM0_bits + GIE);         // Enter LPM0 w/ interrupt
 
 	while (1)
 	{
@@ -207,7 +216,7 @@ __interrupt void TIMERA_ISR(void)
 	rsx = 0;
 	rsy = 0;
 	
-	ADC12CTL0 |= ADC12SC;
+	ADC12CTL0 |= ADC12SC; // Begin another conversion
 	
 	//__bic_SR_register_on_exit(LPM0_bits);     // Exit LPM0
 }
