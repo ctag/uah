8081d7dd3b48ff2cec4c47bc0ffedaf5e558eca0
Christopher Bero <bigbero@gmail.com>
2015-02-06 14:59:50 -0600

    done with lab 03

---
+++ cpe322_324/lab03/behavioral/eight_bit_sub_add.v
@@ -0,0 +1,24 @@
+// Behavioral Representation of Adder/Subtractor Module
+// presented in Part II of Laboratory 3 Experiment
+// B. Earl Wells -- University of Alabama Huntsville, 2015
+// Single -- Top-Level Module
+module eight_bit_sub_add(D_S,B_COUT,A,B,B_CIN,SUB_ADD);
+   output reg [7:0] D_S;
+   output reg B_COUT;
+   input [7:0] A,B;
+   input B_CIN,SUB_ADD;
+
+   reg [8:0] Cbuf;
+	
+   always @(A or B or B_CIN or SUB_ADD) begin
+      if (SUB_ADD) begin
+         Cbuf = A - B - B_CIN;
+      end
+      else begin
+         Cbuf = A + B + B_CIN;
+      end
+      D_S = Cbuf[7:0];
+      B_COUT = Cbuf[8];
+   end
+	 
+endmodule
