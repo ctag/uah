a81d68a8567e95dcafa89f6d9f7deb8ab0fc4108
Christopher Bero <bigbero@gmail.com>
2015-02-16 03:23:04 -0600

    matlab for hw2 cpe381

---
+++ cpe381/hw02/prob2.m
@@ -0,0 +1,22 @@
+% 2
+
+clear all; clf;
+
+% This was not covered in class or in the book.
+load handel.mat;
+
+% Ostensibly, 'y' and 'Fs' are now available.
+
+t = 0:Fs:1;
+
+l = 0.5;
+
+x1 = y;
+x2 = 0.8*(y-l);
+x3 = 0.5*(y-(2*l));
+
+x = x1 + x2 + x3;
+
+sound(x, Fs);
+% Can then be compared against the command: sound(y, Fs);
+
---
+++ cpe381/hw02/prob5.m
@@ -0,0 +1,21 @@
+% 2
+
+clear all;
+
+syms s t u(t);
+
+f = 5*(exp(-2*t))*(cos(8*t))*(u(t));
+
+r = -5:0.01:5;
+
+L = laplace(f,t,s);
+
+plot(t,L);
+
+%zplane();
+
+f = 5*(exp(-4*t))*(cos(8*t))*(u(t));
+
+r = -5:0.01:5;
+
+L = laplace(f,t,s);
