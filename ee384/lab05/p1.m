3af13c159a0bcc78047bf52e938328d37f345bfa
Christopher Bero <bigbero@gmail.com>
2015-06-30 19:57:00 -0500

    lab 5 for ee384

--- ee384/lab05/p1.m
+++ ee384/lab05/p1.m
@@ -2,3 +2,20 @@
 % Christopher Bero
 % EE 384
 
+% P1.a
+% Read and listen to test.wav
+% What do you hear?
+% What is the sampling frequency?
+[sig,fs]=wavread('/home/berocs/Documents/uah/ee384/lab05/test.wav');
+%sound(sig,fs);
+% Audio: "One, two, three."
+% Sampling Frequency: 8000Hz
+
+% P1.b
+% Plot the sound data vs time
+seconds=(length(sig)/fs);
+t=0:(1/fs):seconds-(1/fs);
+plot(t,sig);
+title('test.wav');
+xlabel('Time (t)');
+ylabel('WAV Value (sig)');
